8216f1c5697de6130e8af35e65d2cfab284084d6
Gambleruin <dannylovewargame@gmail.com>
2018-06-09 09:34:06 -0300

    *

--- sum.cpp
+++ sum.cpp
@@ -19,7 +19,15 @@ void sum(int *p, int n, vector <int> d){
 int main(){
 	int accum =0;
 	vector<int> data(N);
+
+	//init the vector
+	for (int j = 0; j < N; ++j) {
+    	data[j] =j;
+
 	memset( &d, 0, sizeof( d ) );
 	sum(&accum, N, data);
+
+	//print out the result
+	cout<<"sum is %d\n"<< accum<<endl;
 	return 0;
 }
