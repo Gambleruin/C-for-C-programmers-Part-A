ee13f97b5ab05983baea9b7342f75a22bd2db6b3
Gambleruin <dannylovewargame@gmail.com>
2018-06-11 21:19:44 -0300

    *

--- dstp_assignment_2.cpp
+++ dstp_assignment_2.cpp
@@ -15,5 +15,12 @@ bool **graph;
 	graph =new bool*[size];
 	for (int i =0; i<size; i++)
 		graph[i] =new bool[size];
-	
+
 //heap created 2d array of Bool
+//next, Density is 0.19
+for(int i =0; i<size; ++i)
+	for(int j =i; j<size; ++j)
+		if(i ==j) graph[i][j] =false; //np loops
+			else graph[i][j] =graph[j][i] =(prob()<0.19);
+
+//density is the probability that an edge exists
